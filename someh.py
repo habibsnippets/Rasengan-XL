02f44a0dd4be8c78ca5904b082668eafa103e4e0
habibsnippets <hc17xdevelopers@gmail.com>
2025-11-22 17:05:34 +0530

    metadata fixed

--- someh.py
+++ someh.py
@@ -1,6 +1,6 @@
 import nbformat
 
-nb = nbformat.read("Rasengan-XL_DORA.ipynb", as_version=4)
+nb = nbformat.read("Rasengan-XL-QLoRA/", as_version=4)
 
 # Remove widgets metadata if broken
 if "widgets" in nb.metadata:
