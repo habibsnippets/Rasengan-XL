8698db8ae1f28c51d45f7a1a083f60542d06fd92
habibsnippets <hc17xdevelopers@gmail.com>
2025-11-22 17:00:52 +0530

    fixing metadata

---
+++ someh.py
@@ -0,0 +1,9 @@
+import nbformat
+
+nb = nbformat.read("Rasengan-XL_DORA.ipynb", as_version=4)
+
+# Remove widgets metadata if broken
+if "widgets" in nb.metadata:
+    del nb.metadata["widgets"]
+
+nbformat.write(nb, "your_notebook_fixed.ipynb")
